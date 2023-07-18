4e15c7215dcdb42cc6cbd2601c52197cd79cf126
John Christian Cabrera <128894207+rasderty01@users.noreply.github.com>
2023-07-18 15:09:38 +0800

    latest

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="min-h-screen p-2 items-center justify-center">
-      <SignUp />;
+      <SignUp afterSignUpUrl={"/"} />;
     </div>
   );
 }
